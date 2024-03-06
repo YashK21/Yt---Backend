94fdbad2fbb8cfc206bee131e06269e2879b43cc
yashk21 <yashk8119@gmail.com>
2024-03-07 00:51:18 +0530

    config setup for all

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 
-app.arguments(
+app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
@@ -13,4 +13,17 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extend: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js" 
+// here we imported userRouter as the name for router in user.routes.js
+// we have given other name then router , it's possible bcz we exported router as default (See the file)
+
+// routes decl
+app.use("/api/v1/users",userRouter) 
+// as soon the /api/v1/users is hit by the client the control goes to userRouter,
+// and then in userRouter the further route is hit so
+// the url becomes - http://localhost:8000/api/v1/users/register - /register comes from userRouter
+// we can have multiple routes like that
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // require("dotenv").config({path:"./env"})
 import dotenv from "dotenv";
 import connDB from "./db/conn.js";
+import {app} from './app.js'
+
 connDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
